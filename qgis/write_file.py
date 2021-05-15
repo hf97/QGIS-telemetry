fd92c9a6dfc7d766138e2b2069a8d78f9bd90be1
HF97 <hugoanf97@gmail.com>
2021-05-15 16:43:26 +0100

    Qgis interface

--- qgis/write_file.py
+++ qgis/write_file.py
@@ -2,9 +2,9 @@ import json
 from datetime import datetime
 import uuid, OpenSSL
 import json
-import datetime
 import random
 import platform
+
 
 ################################################################
 # Usar isto mas nÃ£o sei meter a dar sem ser no qgis
@@ -13,23 +13,13 @@ import platform
 #   QgsSettings,
 #   QgsVectorLayer
 # )
-###############################################################
-# def openProject():
-#     f = open("C:/Users/hugof/Desktop/QGIS-telemetry/qgis/telemetry.json", "r+")
-#     # print(f.read())
-#     # f.seek(0)
-#     obj = json.loads(f.read())
-#     # obj = json.loads("C:/Users/hugof/Desktop/QGIS-telemetry/qgis/telemetry.json")
-#     try:
-#         # obj["actions"].append({"type" : "start"})
-#         obj["actions"].update({"type" : "start"})
-#         json.dump(obj, f, indent=4)
-#         # json.dump(obj, f)
-#         pass
-#     except:
-#         pass
-#     print(obj)
-#     f.close()
+###################################
+# Isto vai buscar a settings do qgis
+# s = QgsSettings()
+# s.value("UI/UITheme")
+# s.value("locale/userLocale")
+###################################
+
 
 # como vamos fazer para o sessionid ser armazendado na execucao e sabermos qual e sempre que se guarda coisas no json
 sessionId = 0
@@ -37,27 +27,22 @@ sessionId = 0
 def openProject():
     # sessionId = uuid.UUID(bytes = OpenSSL.rand.bytes(16))
     now = datetime.now()
-    action_start = {"sessionId": sessionId, "type" : "start", "datetime" : now.isoformat()}
+    try:
+        s = QgsSettings()
+        interface = {}
+        interface["OS"] = platform.system()
+        interface["language"] = s.value("locale/userLocale")
+        interface["locale"] = s.value("locale/globalLocale")
+        interface["uiTheme"] = s.value("UI/UITheme")
+        interface["version"] = Qgis.QGIS_VERSION
+        action_start = {"sessionId": sessionId, "type" : "start", "datetime" : now.isoformat(), "interface": interface}
+    except:
+        action_start = {"sessionId": sessionId, "type" : "start", "datetime" : now.isoformat()}
     try:
         f = open("C:/Users/hugof/Desktop/QGIS-telemetry/qgis/telemetry.json", "r+")
         obj = json.loads(f.read())
         f.close()
-        f = open("C:/Users/hugof/Desktop/QGIS-telemetry/qgis/telemetry.json", "w+")
-        currentPlatform = platform.system()
-        j={}
-        j["sessionId"] = sessionId
-        j["type"] = "open"
-        j["datetime"] = str(x)
-        interface={}
-        interface["OS"] = currentPlatform
-        j["interface"] = interface
-        
-        ###################################
-        # Isto vai buscar a settings do qgis
-        # s = QgsSettings()
-        # s.value("UI/UITheme")
-        # s.value("locale/userLocale")
-        ###################################
+        f = open("C:/Users/hugof/Desktop/QGIS-telemetry/qgis/telemetry.json", "w+")      
         obj["actions"].append(action_start)
         json.dump(obj, f, indent=4)
         f.close()
