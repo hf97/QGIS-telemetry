490315b89aa644b18b292dab9f049e6fe6bf27a9
HF97 <hugoanf97@gmail.com>
2021-05-28 14:17:26 +0100

    Post qgis

--- qgis_telemetry_server/server/models.py
+++ qgis_telemetry_server/server/models.py
@@ -7,47 +7,65 @@ from django.utils import timezone
 class Location(models.Model):
     location_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
+    def __str__(self):
+        return self.name
 
     
 class Telemetry(models.Model):
     telemetry_id = models.AutoField(primary_key=True)
     date_time = models.DateTimeField(default=timezone.now)
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.date_time} {self.location.name}'
 
 
 class Plugin(models.Model):
     plugin_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
     version = models.CharField(max_length=45)
+    def __str__(self):
+        return f'{self.name} {self.version}'
 
 class Provider(models.Model):
     provider_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Os(models.Model):
     os_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Language(models.Model):
     language_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Qgis_version(models.Model):
     qgis_version_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Ui_theme(models.Model):
     ui_theme_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Locale(models.Model):
     locale_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=15)
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Interface(models.Model):
@@ -57,6 +75,8 @@ class Interface(models.Model):
     ui_theme_id = models.ForeignKey(Ui_theme, on_delete=models.CASCADE)
     locale_id = models.ForeignKey(Locale, on_delete=models.CASCADE)
     os_id = models.ForeignKey(Os, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.language_id.name} {self.qgis_version_id.name} {self.ui_theme_id.name} {self.location_id.name} {self.os_id.name}'
 
 
 class Server(models.Model):
@@ -64,6 +84,8 @@ class Server(models.Model):
     date_time = models.DateTimeField(default=timezone.now)
     protocol = models.CharField(max_length=45)
     location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.date_time.name} {self.protocol} {self.location_id.name}'
 
 
 class Action(models.Model):
@@ -74,3 +96,5 @@ class Action(models.Model):
     interface_id = models.ForeignKey(Interface, on_delete=models.CASCADE)
     plugin_id = models.ForeignKey(Plugin, on_delete=models.CASCADE)
     server_id = models.ForeignKey(Server, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.name} {self.telemetry_id.name} {self.provider_id.name} {self.interface_id.name} {self.plugin_id.name} {self.server_id.name}'
