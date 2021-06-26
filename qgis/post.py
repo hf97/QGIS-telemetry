58fe40012aef8d1f964c9a24e6396bade4c12994
HF97 <hugoanf97@gmail.com>
2021-06-26 10:15:17 +0100

    User agent

--- qgis/post.py
+++ qgis/post.py
@@ -2,8 +2,9 @@ import json
 import requests
 # req = QNetworkRequest (QUrl("http://127.0.0.1:8000/jsonfile"))
 # manager=QNetworkAccessManager()
-# url="http://127.0.0.1:8000/jsonfile"
-url="https://qgis-telemetry.herokuapp.com/jsonfile"
+url="http://127.0.0.1:8000/jsonfile"
+# url="https://qgis-telemetry.herokuapp.com/jsonfile"
 f = open("C:/Users/hugof/Desktop/QGIS-telemetry/qgis/telemetry.json", "r+")
 obj = f.read()
-x = requests.post(url, files = dict(telemetry = obj))
+x = requests.post(url, files = dict(telemetry = obj), headers = {'User-Agent': 'QGIS-Telemetry'})
+# x = requests.post(url, files = dict(telemetry = obj))
--- qgis_telemetry_server/server/views.py
+++ qgis_telemetry_server/server/views.py
@@ -21,31 +21,35 @@ def index(request):
 @csrf_exempt
 def jsonfile(request):
     if request.method == "POST":
-        reader = geoip2.database.Reader("./GeoLite2-Country_20210615/GeoLite2-Country.mmdb")
         try:
-            ip = request.META.get('HTTP_X_FORWARDED_FOR')
+            if request.META['HTTP_USER_AGENT'] == "QGIS-Telemetry":
+                reader = geoip2.database.Reader("./GeoLite2-Country_20210615/GeoLite2-Country.mmdb")
+                try:
+                    ip = request.META.get('HTTP_X_FORWARDED_FOR')
+                except:
+                    ip = request.META.get('REMOTE_ADDR')
+                try:
+                    response = reader.country(ip)
+                    ip_location = response.registered_country.name['en']
+                except:
+                    ip_location = "None"
+                print("ip:", ip, "ip_location:", ip_location)
+
+                
+                for filename in request.FILES:
+                    uploaded_file = request.FILES[filename]
+                    dictjson = json.loads(uploaded_file.read())
+                    info = {}
+                    for action in dictjson['actions']:
+                        if action['sessionId'] in info:
+                            info[action['sessionId']].append(action)
+                        else:
+                            info[action['sessionId']] = []
+                            info[action['sessionId']].append(dict(action))
+
+                    parse_files(ip_location, info)
+
+                    # default_storage.save('./telemetry/'+uploaded_file.name+".json", uploaded_file)
         except:
-            ip = request.META.get('REMOTE_ADDR')
-        try:
-            response = reader.country(ip)
-            ip_location = response.registered_country.name['en']
-        except:
-            ip_location = "None"
-        print("ip:", ip, "ip_location:", ip_location)
-
-        
-        for filename in request.FILES:
-            uploaded_file = request.FILES[filename]
-            dictjson = json.loads(uploaded_file.read())
-            info = {}
-            for action in dictjson['actions']:
-                if action['sessionId'] in info:
-                    info[action['sessionId']].append(action)
-                else:
-                    info[action['sessionId']] = []
-                    info[action['sessionId']].append(dict(action))
-
-            parse_files(ip_location, info)
-
-            # default_storage.save('./telemetry/'+uploaded_file.name+".json", uploaded_file)
+            print("Tentativa de post falhada")
         return render(request, "server/index.html")
